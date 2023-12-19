41fdabc78d03b04662d9efb359ddbfbcb60d86fe
Ainara <ainarabliz@gmail.com>
2023-12-19 02:11:17 +0100

    pop-up created and actions favourites design finished

---
+++ src/app/delete-success/delete-success.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-delete-success',
+  standalone: true,
+  imports: [],
+  templateUrl: './delete-success.component.html',
+  styleUrl: './delete-success.component.css'
+})
+export class DeleteSuccessComponent {
+
+}
