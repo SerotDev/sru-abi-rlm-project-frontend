66cc6b6e760be6b2792075c5ddb925a63bda59a7
Sergi Rodriguez Utge <sergidpm@hotmail.com>
2024-01-10 19:25:58 +0100

    home component finished

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -9,5 +9,6 @@ import { RouterLink } from '@angular/router';
   styleUrl: './search.component.css'
 })
 export class SearchComponent {
+  //https://sru-abi-rlm-project-backend-production.up.railway.app/api/hotels/%7Bpage%7D%7Bsize%7D%7BidTown%7D%7Bsearch%7D%7BminStarRatingAvg%7D%7BminNumberRooms%7D%7BminPrice%7D%7BmaxPrice%7D%7BidServices%7D?page=0&size=2&search=reus
   //encodeURIComponent(myUrl);
 }
